ef141a13205c32fa349b4b8659fec36a2eaea985
Gayette <gaelventurin@hotmail.fr>
2016-11-10 16:25:21 +0100

    resolution prob close modal

--- js/modal.js
+++ js/modal.js
@@ -17,3 +17,10 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
+/* When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+*/
